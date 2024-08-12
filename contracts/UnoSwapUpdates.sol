e2cee5e98dfb197c861a0e683bdf9fb4e6931931
YaIWon <158149598+YaIWon@users.noreply.github.com>
2024-08-12 02:40:51 -0700

    Update UnoSwapUpdates.sol

--- contracts/UnoSwapUpdates.sol
+++ contracts/UnoSwapUpdates.sol
@@ -5,137 +5,140 @@ import './UnoSwapDataObjects.sol';
 
 contract UnoSwapUpdates is UnoSwapDataObjects {
 
- function updateOwner(address newOwner, uint256 itemId) internal {
- require(msg.sender != address(0), "address 0:owner");
- MarketItem storage marketItem =idToMarketItem[itemId];
- marketItem.currentOwner = newOwner;
- }
- function updateSeller(address newSeller, uint256 itemId) internal {
-   require(msg.sender != address(0), "address 0: seller");
-   MarketItem storage marketItem =idToMarketItem[itemId];
-   marketItem.currentSeller = newSeller;
- }
- function updateListSwapStatus(bool listStatus, uint256 itemId) internal{
-   require(msg.sender != address(0), "address 0: listStatus");
-   MarketItem storage marketItem =idToMarketItem[itemId];
-   marketItem.listForSwap = listStatus;
- }
- function updateAnySwapAvailable(bool swapAvailabilityStatus, uint256 itemId) internal{
-   require(msg.sender != address(0), "address 0: swapAvailabilityStatus");
-   MarketItem storage marketItem =idToMarketItem[itemId];
-   marketItem.anySwapAvailable = swapAvailabilityStatus;
- }
- function updateSwappedStatus(bool swapStatus, uint256 itemId) internal {
-   require(msg.sender != address(0), "address 0: swapped");
-   MarketItem storage marketItem =idToMarketItem[itemId];
-   marketItem.swapped = swapStatus;
- }
- function updateApprovalStatus(bool approvalStatus, uint256 itemId) internal {
-   require(msg.sender != address(0), "address 0: approval");
-   ApprovalNeeded storage approvalNeeded = idToApproval[itemId];
-   approvalNeeded.approved = approvalStatus;
- }
- function updateApprovalNeeded(bool approval, uint256 itemId) internal {
-   require(msg.sender != address(0), "address 0: approval");
-   ApprovalNeeded storage approvalNeeded = idToApproval[itemId];
-   approvalNeeded.approvalneeded = approval;
- }
+    function updateOwner(address newOwner, uint256 itemId) internal {
+        require(msg.sender != address(0), "address 0:owner");
+        MarketItem storage marketItem = idToMarketItem[itemId];
+        marketItem.currentOwner = newOwner;
+    }
 
- function createItem(
-   uint256 itemId,
-   uint256 tokenId,
-   address tokenAddress,
-   string memory URI,
-   address currentSeller,
-   address currentOwner,
-   uint256 price,
-   bool listForSwap,
-   bool anySwapAvailable,
-   bool swapped
-   ) internal {
+    function updateSeller(address newSeller, uint256 itemId) internal {
+        require(msg.sender != address(0), "address 0: seller");
+        MarketItem storage marketItem = idToMarketItem[itemId];
+        marketItem.seller = newSeller;
+    }
 
-     idToMarketItem[itemId] = MarketItem (
-       itemId,
-        tokenId,
-        tokenAddress,
-        URI,
-        currentSeller,
-        currentOwner,
-        price,
-        listForSwap,
-        anySwapAvailable,
-        swapped
-       );
+    function updateListSwapStatus(bool listStatus, uint256 itemId) internal {
+        require(msg.sender != address(0), "address 0: listStatus");
+        MarketItem storage marketItem = idToMarketItem[itemId];
+        marketItem.listForSwap = listStatus;
+    }
 
-     emit MarketItemCreated(
-       itemId,
-       tokenId,
-       tokenAddress,
-       URI,
-       currentSeller,
-       currentOwner,
-       price,
-       listForSwap,
-       anySwapAvailable,
-       swapped
-       );
+    function updateAnySwapAvailable(bool swapAvailabilityStatus, uint256 itemId) internal {
+        require(msg.sender != address(0), "address 0: swapAvailabilityStatus");
+        MarketItem storage marketItem = idToMarketItem[itemId];
+        marketItem.anySwapAvailable = swapAvailabilityStatus;
+    }
 
- }
+    function updateSwappedStatus(bool swapStatus, uint256 itemId) internal {
+        require(msg.sender != address(0), "address 0: swapped");
+        MarketItem storage marketItem = idToMarketItem[itemId];
+        marketItem.swappedStatus = swapStatus;
+    }
 
+    function updateApprovalStatus(bool approvalStatus, uint256 approvalId) internal {
+        require(msg.sender != address(0), "address 0: approval");
+        ApprovalNeeded storage approvalNeeded = idToApproval[approvalId];
+        approvalNeeded.approved = approvalStatus;
+    }
 
- function updateItem(
-   uint256 itemId,
-   uint256 tokenId,
-   address tokenAddress,
-   string memory URI,
-   address currentSeller,
-   address currentOwner,
-   uint256 price,
-   bool listForSwap,
-   bool anySwapAvailable,
-   bool swapped
-   ) internal {
+    function updateApprovalNeeded(bool approval, uint256 approvalId) internal {
+        require(msg.sender != address(0), "address 0: approval");
+        ApprovalNeeded storage approvalNeeded = idToApproval[approvalId];
+        approvalNeeded.approvalNeeded = approval;
+    }
 
-     idToMarketItem[itemId] = MarketItem (
-       itemId,
-        tokenId,
-        tokenAddress,
-        URI,
-        currentSeller,
-        currentOwner,
-        price,
-        listForSwap,
-        anySwapAvailable,
-        swapped
-       );
- }
+    function createItem(
+        uint256 itemId,
+        uint256 tokenId,
+        address tokenAddress,
+        string memory URI,
+        address currentSeller,
+        address currentOwner,
+        uint256 price,
+        bool listForSwap,
+        bool anySwapAvailable,
+        bool swapped
+    ) internal {
 
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            tokenId,
+            tokenAddress,
+            URI,
+            currentSeller,
+            currentOwner,
+            price,
+            listForSwap,
+            anySwapAvailable,
+            swapped
+        );
 
- function createUpdateApproval(
-  bool approvalneeded,
-  uint256 approvalCount,
-  uint256 itemId1,
-  uint256 itemId2,
-  address owner1,
-  address owner2,
-  uint256 tokenId1,
-  uint256 tokenId2,
-  bool approved,
-  bool requestCanceled
-  )
+        emit MarketItemCreated(
+            itemId,
+            tokenId,
+            tokenAddress,
+            URI,
+            currentSeller,
+            currentOwner,
+            price,
+            listForSwap,
+            anySwapAvailable,
+            swapped
+        );
+    }
 
-  internal {
-   idToApproval[approvalCount] = ApprovalNeeded (
-     approvalneeded,
-      approvalCount,
-      tokenId1,
-      tokenId2,
-      owner1,
-      owner2,
-      tokenId1,
-      tokenId2,
-      approved,
-      requestCanceled
-     );
- }
+    function updateItem(
+        uint256 itemId,
+        uint256 tokenId,
+        address tokenAddress,
+        string memory URI,
+        address currentSeller,
+        address currentOwner,
+        uint256 price,
+        bool listForSwap,
+        bool anySwapAvailable,
+        bool swapped
+    ) internal {
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            tokenId,
+            tokenAddress,
+            URI,
+            currentSeller,
+            currentOwner,
+            price,
+            listForSwap,
+            anySwapAvailable,
+            swapped
+        );
+    }
+
+    function createUpdateApproval(
+        bool approvalNeeded,
+        uint256 approvalCount,
+        uint256 itemId1,
+        uint256 itemId2,
+        address owner1,
+        address owner2,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        bool approved,
+        bool requestCanceled
+    )
+
+    internal {
+        idToApproval[approvalCount] = ApprovalNeeded(
+            approvalNeeded,
+            approvalCount,
+            tokenId1,
+            tokenId2,
+            owner1,
+            owner2,
+            approved,
+            requestCanceled,
+            false,
+            false
+        );
+    }
 }
