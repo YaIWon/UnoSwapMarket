e8650a081929aa9b98d96d9b0c7f3acff8682cee
YaIWon <158149598+YaIWon@users.noreply.github.com>
2024-08-12 02:35:27 -0700

    Update UnoSwapConnector.sol

--- contracts/UnoSwapConnector.sol
+++ contracts/UnoSwapConnector.sol
@@ -6,11 +6,13 @@ import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 import "./UnoSwapViews.sol";
 import "./UnoSwapUpdates.sol";
 
+contract UnoSwapConnector is ERC721URIStorageUpgradeable, UnoSwapUpdates, ReentrancyGuardUpgradeable, UnoSwapViews, UnoSwapMarket {
 
-contract UnoSwapConnector is ERC721URIStorageUpgradeable ,UnoSwapUpdates ,ReentrancyGuardUpgradeable,UnoSwapViews {
+    constructor(string memory Name, string memory Symbol) initializer ERC721Upgradeable() {
+        __ERC721_init(Name, Symbol);
+    }
 
-  constructor(string memory Name, string memory Symbol) initializer ERC721Upgradeable() {
-__ERC721_init(Name, Symbol);
-  }
-
+    function swapToken(uint256 itemId1, uint256 itemId2, uint256 priceRatio) public {
+        swapTokenERC20(itemId1, itemId2, priceRatio);
+    }
 }
